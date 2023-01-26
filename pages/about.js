da588f26d0796c1f106e1e652ac6b8d0ec8a5dd5
omnomrose <maryrosenguyenbusiness@gmail.com>
2023-01-26 15:44:54 -0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function About(){
+    return(
+        <>About
+            <Link href="/">
+                Go Back
+            </Link>
+        </>
+    )
+}
